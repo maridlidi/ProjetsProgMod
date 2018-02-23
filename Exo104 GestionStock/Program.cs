32b24ee4bf08dcb7d48cf05bf3aa6014a431ef0a
cfib18 <cfib18@pedagogie.cegep-chicoutimi.qc.ca>
2018-02-23 11:23:57 -0500

    mise a jour

---
+++ Exo104 GestionStock/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo104_GestionStock
+{
+    public struct arcticle
+    {
+        public string nomArcticle;
+        public int numeroArcticle;
+        public double prixVente;
+        public double prixPrecedent;
+        public int quantite;
+        public List<string> reference;
+
+        public arcticle(string NomArcticle, int NumeroArcticle,)
+        
+    }
+
+    public struct stock
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
